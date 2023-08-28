a94b0a1c96c0f5ca18892d50bc83d7142d69b90f
Daniel Nuez <daniel.nuez@sngular.com>
2023-08-28 21:43:45 +0200

    Eslint dependency warning fixed

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import { ProductsActions } from "../../actions/products"
-import { ProductsRow } from "../../types/product"
-import Header from "../header/Header"
+import { useCallback, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ProductsActions } from '../../actions/products'
+import { ProductsRow } from '../../types/product'
+import Header from '../header/Header'
 import './editor.css'
-import ProductRow from "../row/ProductRow"
+import ProductRow from '../row/ProductRow'
 
 type EditorParams = {
   ids: string
@@ -14,22 +14,22 @@ const Editor: React.FC = () => {
   const { ids } = useParams<EditorParams>()
   const [productsRows, setProductsRows] = useState<ProductsRow[]>([])
 
-  useEffect(() => {
-    retrieveProducts()
-  }, [ids])
-
-  const retrieveProducts = async (): Promise<void> => {    
-    const newProducts: ProductsRow[] = await ProductsActions.retrieveProducts(ids!)        
+  const retrieveProducts = useCallback(async () => {
+    const newProducts: ProductsRow[] = await ProductsActions.retrieveProducts(ids!)
     setProductsRows(() => {
       return newProducts.map((product) => product)
     })
-  }
+  }, [ids])
+
+  useEffect(() => {
+    retrieveProducts()
+  }, [retrieveProducts])
 
   return (
     <>
       <Header />
-      {productsRows.map((row, index) => {        
-        return <ProductRow products={row} key={`product-row-${index}`}/>
+      {productsRows.map((row, index) => {
+        return <ProductRow products={row} key={`product-row-${index}`} />
       })}
     </>
   )
